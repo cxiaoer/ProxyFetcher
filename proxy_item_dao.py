64ab6eaa7e1f0f9ae7bc3a093640ba216525fa12
cxiaoer <chen_wen@hotmail.com>
2016-08-26 19:12:34 +0800

    sql写错了

--- proxy_item_dao.py
+++ proxy_item_dao.py
@@ -77,16 +77,16 @@ def update_proxy_status(is_success, ip_info_list):
                 update_sql = 'update T_IP_Proxies set Status = %s, ' \
                              'NextTestTime = %s , SuccessedTestTimes = ' \
                              'SuccessedTestTimes +1 ' \
-                             'where Ip = %s and Port = %s' % (ip_info['status'],
-                                                              ip_info['next_test_time'], ip_info['ip'],
-                                                              ip_info['port'])
+                             'where Ip = %s and Port = %s' % (ip_info.status,
+                                                              ip_info.next_test_time, ip_info.ip,
+                                                              ip_info.port)
             else:
                 update_sql = 'update T_IP_Proxies set Status = %s, ' \
                              'NextTestTime = %s , FailedTestTimes = ' \
                              'FailedTestTimes +1 ' \
-                             'where Ip = %s and Port = %s' % (ip_info['status'],
-                                                              ip_info['NextTestTime'], ip_info['ip'],
-                                                              ip_info['port'])
+                             'where Ip = %s and Port = %s' % (ip_info.status,
+                                                              ip_info.NextTestTime, ip_info.ip,
+                                                              ip_info.port)
             cursor.execute(update_sql)
             connection.commit()
     except MySQLdb.Error as error:
