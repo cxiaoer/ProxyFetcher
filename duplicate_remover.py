2582aa45e1e731d16a17c8b4450fa1762d12f566
cxiaoer <chen_wen@hotmail.com>
2016-08-31 15:34:46 +0800

    完成最简单的set去重逻辑

--- duplicate_remover.py
+++ duplicate_remover.py
@@ -1,6 +1,10 @@
 # coding:utf-8
 
 import hashlib
+from threading import RLock
+
+url_md5_set = set()  # url的md5集合
+__lock = RLock()  # 将查询和添加set封装成原子操作的锁
 
 
 # url的md5的32位16进制表示
@@ -13,7 +17,14 @@ def __url_md5(url):
 
 # set 去重
 def set_duplicate_remover(url):
-    pass
+    url_md5 = __url_md5(url=url)
+    with __lock:
+        if url_md5 in url_md5_set:
+            is_url_crawled = True
+        else:
+            is_url_crawled = False
+            url_md5_set.add(url_md5)
+    return is_url_crawled
 
 
 # redis 去重
