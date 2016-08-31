f24480c6a6670a71e39bc0c60414b8ae1c4beac4
cxiaoer <chen_wen@hotmail.com>
2016-08-31 16:22:21 +0800

    增加几个user_agent

--- configs/user_agent_config.py
+++ configs/user_agent_config.py
@@ -9,6 +9,18 @@ user_agent_list.append('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) '
 user_agent_list.append('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) '
                        'AppleWebKit/537.36 (KHTML, like Gecko) '
                        'Chrome/51.0.2704.106 Safari/537.36')
+user_agent_list.append('Mozilla/5.0 (Windows NT 6.3; Win64, x64; '
+                       'Trident/7.0; rv:11.0) like Gecko')
+user_agent_list.append('Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                       'AppleWebKit/537.36 (KHTML, like Gecko) '
+                       'Chrome/44.0.2403.157 Safari/537.36')
+user_agent_list.append('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; '
+                       'rv:40.0) Gecko/20100101 Firefox/40.0')
+user_agent_list.append('Mozilla/5.0 (Windows NT 10.0; Win64; x64; '
+                       'rv:40.0) Gecko/20100101 Firefox/40.0')
+user_agent_list.append('Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                       'AppleWebKit/537.36 (KHTML, like Gecko) '
+                       'Chrome/42.0.2311.135 Safari/537.36 Edge/12.10136')
 
 
 # 随机获取一个user_agent
