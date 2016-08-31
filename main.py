7133ba2b23e1b4994a1688f6d558de0361087568
cxiaoer <chen_wen@hotmail.com>
2016-08-31 14:50:43 +0800

    行程完整的程序

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 # coding:utf-8
 
+from fetch_proxy_thread import fetch, init
+from test_proxy_thread import test
+
 if __name__ == '__main__':
-    pass
+    init()  # 初始化任务
+    fetch()  # 开始抓取代理
+    test()  # 检测代理
