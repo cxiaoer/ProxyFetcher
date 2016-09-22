e430da0db16e0e4bd7bdbbb6a1ee4753956c4ad0
cxiaoer <chen_wen@hotmail.com>
2016-09-22 13:53:35 +0800

    使用sqlachemy , 抛弃peewee； API接口使用及其反人类

--- items/ProxyItem.py
+++ items/ProxyItem.py
@@ -3,44 +3,57 @@
 代理对象的类表示
 """
 
-from peewee import *
+from sqlalchemy import Column, BigInteger, Integer, String, create_engine, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.expression import text
+from sqlalchemy.orm import sessionmaker
 from configs import db_config
-import datetime
 
-db = MySQLDatabase(db_config.db,
-                   user=db_config.db_username,
-                   host=db_config.db_host,
-                   password=db_config.db_password)
+# 对象的基类
+Base = declarative_base()
+# 初始化数据库连接
+mysql_str_format = 'mysql+pymysql://{username}:{password}@{host}:3306/{db}'
+engine = create_engine(mysql_str_format.format(username=db_config.db_username,
+                                               password=db_config.db_password,
+                                               host=db_config.db_host,
+                                               db=db_config.db))
 
 
-class ProxyItem(Model):
+# 会话
+DBSession = sessionmaker(bind=engine)
+
+
+class ProxyItem(Base):
     """抓取任务的抽象"""
-    record_id = BigIntegerField(db_column='RecordId', primary_key=True)
-    ip = CharField(db_column='Ip')
-    port = CharField(db_column='Port')
-    proxy_type = CharField(db_column='ProxyType')
-    location = CharField(db_column='Location')
-    status = IntegerField(db_column='Status')
-    success_test_times = IntegerField(db_column='SuccessedTestTimes')
-    failed_test_times = IntegerField(db_column='FailedTestTimes')
-    next_test_time = DateTimeField(db_column='NextTestTime', default=datetime.datetime.now())
-    last_modify_time = DateTimeField(db_column='LastModifyTime', default=datetime.datetime.now())
-    create_time = DateTimeField(db_column='CreateTime', default=datetime.datetime.now())
 
-    class Meta:
-        """Meta"""
-        database = db
-        db_table = 'T_IP_Proxies'
-        indexes = (
-            # ip 和 port 的唯一索引
-            (('ip', 'port'), True),
-        )
+    # 连接对应的表名
+    __tablename__ = 'T_IP_Proxies'
+
+    record_id = Column('RecordId', BigInteger, primary_key=True)
+    ip = Column('Ip', String)
+    port = Column('Port', String)
+    proxy_type = Column('ProxyType', String)
+    location = Column('Location', String, nullable=True)
+    status = Column('Status', Integer, server_default=text('0'))
+    success_test_times = Column(
+        'SuccessedTestTimes', Integer, server_default=text('0'))
+    failed_test_times = Column(
+        'FailedTestTimes', Integer, server_default=text('0'))
+    next_test_time = Column('NextTestTime', DateTime,
+                            server_default=text('now()'))
+    last_modify_time = Column(
+        'LastModifyTime', DateTime, server_default=text('now()'))
+    create_time = Column('CreateTime', DateTime,
+                         server_default=text('now()'))
 
 
 if __name__ == '__main__':
-    item = ProxyItem.select().where(ProxyItem.ip == '182.89.7.212' and ProxyItem.proxy_type == 'HTTP').get()
-    print(item.ip)
-    item1 = ProxyItem(ip='test', port='test')
-    item2 = ProxyItem(ip='test', port='test')
-    item1.save()
-    item2.save()
+    session = DBSession()
+    proxy_items = session.query(ProxyItem).filter(
+        ProxyItem.ip == '1.0.247.171').all()
+    print(type(proxy_items))
+    # print(proxy_item.ip)
+    for item in proxy_items:
+        print(type(item))
+        print(item.ip)
+    session.close()
