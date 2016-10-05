beb2d9b4af24cea2f65b6645d281b0c671e39483
cxiaoer <chen_wen@hotmail.com>
2016-10-05 08:44:46 +0800

    start refactor parser part

---
+++ items/ExtractorConfigItem.py
@@ -0,0 +1,11 @@
+"""解析配置对应的item
+"""
+# coding:utf-8
+
+
+class ExtractorConifgItem(object):
+    """解析配置描述bean"""
+
+    def __init__(self, arg):
+        super(ExtractorConifgItem, self).__init__()
+        self.arg = arg
---
+++ parser.py
@@ -0,0 +1,18 @@
+# coding:utf-8
+"""
+解析逻辑
+"""
+
+
+def parser(context, html, extractor_info):
+    """解析html
+
+    Args:
+        context (TYPE): 解析过程中的上下文
+        html (TYPE): 抓取的html文本内容
+        extractor_info (TYPE): 解析的配置
+
+    Returns:
+        TYPE: 分组的一个list列表
+    """
+    pass
